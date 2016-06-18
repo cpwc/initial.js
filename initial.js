1c94aad1c4e255198bec8c287ae00f2afbe2f8cc
Wei Cheng <calvinpohwc@gmail.com>
2016-06-18 19:50:35 +0800

    Updated to use randomColor library instead.

--- initial.js
+++ initial.js
@@ -1,23 +1,13 @@
 (function(factory) {
   if (typeof define === 'function' && define.amd) {
-    define(['jquery'], factory);
+    define(['jquery', 'randomcolor'], factory);
   } else if (typeof module === 'object' && module.exports) {
-    module.exports = function(root, jQuery) {
-      if (jQuery === undefined) {
-        if (typeof window !== 'undefined') {
-          jQuery = require('jquery');
-        } else {
-          jQuery = require('jquery')(root);
-        }
-      }
-      factory(jQuery);
-      return jQuery;
-    };
+    module.exports = factory(require('jquery'), require('randomcolor'));
   } else {
     // Browser globals
-    factory(jQuery);
+    factory(jQuery, randomColor);
   }
-}(function($) {
+}(function($, randomColor) {
   $.fn.initial = function(options) {
     return this.each(function() {
 
@@ -29,8 +19,6 @@
         charCount: 1,
         wordCount: 2,
         textColor: '#ffffff',
-        saturation: 0.8,
-        brightness: 0.5,
         height: 100,
         width: 100,
         fontSize: 60,
@@ -73,107 +61,15 @@
         'font-size': settings.fontSize + 'px'
       });
 
-      function unique(name) {
-        return name.split('').map(function(v, k) {
-          return v.charCodeAt(0) * Math.pow(2, k);
-        }).reduce(function(v1, v2) {
-          return v1 + v2;
-        }) / Math.pow(3, name.length);
-      }
-
-      function normalize(num) {
-        var border = 3;
-
-        if (num < border) {
-          while (num < border) {
-            num *= 2 * border;
-          }
-        } else {
-          while (parseInt(num) > border) {
-            num /= 2 * border;
-          }
-        }
-
-        return num;
-      }
-
-      function distribute(x, sigma) {
-        return (Math.sin(x * sigma) * Math.cos(x / sigma) * 0.5) + 0.5;
-      }
-
-      function hsv2rgb(hue, saturation, brightness) {
-        var chroma = brightness * saturation;
-        var faceColor = {
-          red: 0,
-          green: 0,
-          blue: 0
-        };
-        var hp = hue * 6;
-        var x = chroma * (1 - Math.abs((hp % 2) - 1));
-        switch (Math.floor(hp)) {
-          case 0:
-            faceColor.red = chroma;
-            faceColor.green = x;
-            break;
-
-          case 1:
-            faceColor.red = x;
-            faceColor.green = chroma;
-            break;
-
-          case 2:
-            faceColor.green = chroma;
-            faceColor.blue = x;
-            break;
-
-          case 3:
-            faceColor.green = x;
-            faceColor.blue = chroma;
-            break;
-
-          case 4:
-            faceColor.red = x;
-            faceColor.green = chroma;
-            break;
-
-          case 5:
-          case 6:
-            faceColor.red = chroma;
-            faceColor.green = x;
-            break;
-        }
-
-        var m = brightness - chroma;
-        var color = {
-          red: Math.floor((faceColor.red + m) * 256),
-          green: Math.floor((faceColor.green + m) * 256),
-          blue: Math.floor((faceColor.blue + m) * 256)
-        };
-
-        var hex = {
-          red: (color.red < 16 ? '0' : '') + Number(color.red).toString(16),
-          green: (color.green < 16 ? '0' : '') + Number(color.green).toString(16),
-          blue: (color.blue < 16 ? '0' : '') + Number(color.blue).toString(16)
-        };
-
-        return hex.red + hex.green + hex.blue;
-      }
-
-      var sigma = normalize(unique(settings.name)) * (settings.seed + 1);
-
-      var color = {
-        hue: distribute(settings.name.length, sigma),
-        saturation: settings.saturation, // 0: White
-        brightness: settings.brightness // 0: Black
-      };
-
       var svg = $('<svg></svg>').attr({
         'xmlns': 'http://www.w3.org/2000/svg',
         'pointer-events': 'none',
         'width': settings.width,
         'height': settings.height
       }).css({
-        'background-color': '#' + hsv2rgb(color.hue, color.saturation, color.brightness),
+        'background-color': randomColor({
+          seed: settings.name
+        }),
         'width': settings.width + 'px',
         'height': settings.height + 'px',
         'border-radius': settings.radius + 'px',
